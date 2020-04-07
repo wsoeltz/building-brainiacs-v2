2ceee421e2b4ad9c789718a4b207197730c733df
Travis CI User <travis@example.org>
2020-04-07 03:19:23 +0000

    Updates

---
+++ component---src-pages-404-js-ef2d5cea2ce04ded2618.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("X3x9"),u=t("LbRr"),c=t("W/9C"),o=t("3CE+");n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement(u.a,null),l.a.createElement(r.a,null,l.a.createElement("h1",null,"404: Page not found")),l.a.createElement(c.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ef2d5cea2ce04ded2618.js.map
