992b839c103b70528752e3628b0bc6ddee23ee2a
Kyle Soeltz <wsoeltz@gmail.com>
2020-04-06 23:05:15 -0400

    Remove broken image path and rebuild package-lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `building-brainiacs-v2`,
+        short_name: `building-brainiacs`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1475a0`,
+        theme_color: `#1475a0`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
